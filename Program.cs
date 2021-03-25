82e7f988ad2d3b1b124d37a6457564d9db9a857d
Asmoww <68480128+Asmoww@users.noreply.github.com>
2021-03-26 00:57:18 +0200

    Added AFK and timer, maybe made stuff smoother

--- Program.cs
+++ Program.cs
@@ -3,23 +3,61 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Management;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
+using System.Windows.Forms;
 
 namespace MultiClient
 {
     class G
     {
         public static bool log = false;
+        public static bool afk = false;
     }
     class Program
     {
+        private const int SW_MINIMIZE = 6;
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern void SwitchToThisWindow(IntPtr hWnd, bool fAltTab);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        [DllImport("user32.dll")]
+        static extern int SetWindowText(IntPtr hWnd, string text);
+
+        public static Process GetForegroundProcess()
+        {
+            uint processID = 0;
+            IntPtr hWnd = GetForegroundWindow(); 
+            uint threadID = GetWindowThreadProcessId(hWnd, out processID); 
+            Process fgProc = Process.GetProcessById(Convert.ToInt32(processID)); 
+            return fgProc;
+        }
+
         static void Main()
         {
             Handle h = new Handle();
             Task.Run(() => DetectClient());
+            Task.Run(() => Afk());
+            Task.Run(() => Timer());
         start:
             string args = Console.ReadLine();
             if (args == "run")
@@ -32,11 +70,23 @@ namespace MultiClient
                 G.log = !G.log;
                 if (G.log)
                 {
-                    Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] Logging enabled.");
+                    Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] Logs enabled.");
                 }
                 else
                 {
-                    Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] Logging disabled.");
+                    Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] Logs disabled.");
+                }
+            }
+            else if (args == "afk")
+            {
+                G.afk = !G.afk;
+                if (G.afk)
+                {
+                    Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] AFK mode on.");
+                }
+                else
+                {
+                    Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] AFK mode off.");
                 }
             }
             else if (args == "clean")
@@ -80,7 +130,7 @@ namespace MultiClient
             {
                 if (clientCount - rblxClients.Length > 1)
                 {
-                    Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] {rblxClients.Length - clientCount} clients were closed. {rblxClients.Length} Client(s) are open.");
+                    Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] {clientCount - rblxClients.Length} clients were closed. {rblxClients.Length} Client(s) are open.");
                 }
                 else
                 {
@@ -88,8 +138,73 @@ namespace MultiClient
                 }
             }
             clientCount = rblxClients.Length;
-            Thread.Sleep(500);
+            Thread.Sleep(100);
             goto detect;
+        }
+
+        public async static Task Afk()
+        {
+            const uint WM_KEYDOWN = 0x100;
+            const uint WM_KEYUP = 0x0101;
+        start:
+            Thread.Sleep(2000);
+            Process[] rblxClients = Process.GetProcessesByName("RobloxPlayerBeta");
+            if (G.afk)
+            {
+                foreach(Process p in rblxClients)
+                {
+                    if (GetForegroundProcess().Id == p.Id)
+                    {
+                        if (G.log) Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] Process {p.Id} in foreground, skipping.");
+                    }
+                    else
+                    {
+                        if (G.log) Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] Sending AFK input to {p.Id}...");
+                        IntPtr windowHandle = p.MainWindowHandle;
+                        Thread.Sleep(20);
+                        SwitchToThisWindow(windowHandle, true);
+                        SetForegroundWindow(windowHandle);
+                        PostMessage(windowHandle, WM_KEYDOWN, (IntPtr)(Keys.Divide), IntPtr.Zero);
+                        Thread.Sleep(20);
+                        PostMessage(windowHandle, WM_KEYUP, (IntPtr)(Keys.Divide), IntPtr.Zero);
+                        Thread.Sleep(20);
+                        ShowWindow(windowHandle, SW_MINIMIZE);
+                        Thread.Sleep(20);
+                    }
+                }
+            }
+            goto start;
+        }
+
+        public async static Task Timer()
+        {
+        start:
+            Thread.Sleep(1000);
+            Process[] rblxClients = Process.GetProcessesByName("RobloxPlayerBeta");
+            foreach (Process p in rblxClients)
+            {
+                if (!p.HasExited)
+                {
+                    try
+                    {
+                        if (p.MainWindowTitle == "Roblox" && p.ProcessName == "RobloxPlayerBeta")
+                        {
+                            SetWindowText(p.MainWindowHandle, "Roblox - 1 seconds");
+                        }
+                        else
+                        {
+                            int windowTime = Int32.Parse(p.MainWindowTitle.Replace("Roblox - ", "").Replace(" seconds", "")) + 1;
+                            SetWindowText(p.MainWindowHandle, "Roblox - " + windowTime + " seconds");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        //Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] ERROR: Unable to set the title for clients. (Client may be closing?)");
+                    }
+                }
+                if (G.log) Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] Client is probably closing, ignoring.");
+            }
+            goto start;
         }
     }
 }
